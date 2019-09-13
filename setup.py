52765af5f0bb56b2080e05949ce9c1c34bff18f1
cullinap <cullinap@gmail.com>
2019-09-12 20:56:26 -0400

    change setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='useful_stuff',
-    version='0.0.6',
+    version='0.0.7',
     author='patrick',
     classifiers=[
         'Programming Language :: Python :: 3',
